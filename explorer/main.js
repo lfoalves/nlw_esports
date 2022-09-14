0c425b2d4892cb82b418d9923656f997eb2d895c
Luiz Fernando de Oliveira Alves <lfoalves.dev@gmail.com>
2022-09-14 19:51:54 -0400

    explorer

---
+++ explorer/main.js
@@ -0,0 +1,12 @@
+// const imagesGames = document.querySelectorAll('main section:nth-child(1) .content ul li img')
+// const sectionGames = document.querySelector('main section:nth-child(1) .content ul');
+// const figure = document.createElement('figure');
+// const gamesList = document.querySelectorAll('main section:nth-child(1) .content ul li a');
+// console.log(imagesGames, sectionGames, gamesList)
+
+// gamesList.forEach(listAncora => {
+//   imagesGames.forEach(image => {
+//     figure.append(image)
+//     listAncora.append(figure)
+//   })
+// })
