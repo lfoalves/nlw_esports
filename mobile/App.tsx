43069fd8f7e445f914c76cedf71b0b2b47a6eba6
Luiz Fernando de Oliveira Alves <lfoalves.dev@gmail.com>
2022-09-16 22:14:45 -0400

    mobile server integration

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -7,7 +7,7 @@ import {
   Inter_900Black
 } from '@expo-google-fonts/inter';
 
-import { Home } from './src/screens/Home';
+import { Routes } from './src/routes';
 import { Brackground } from './src/components/Brackground';
 import { Loading } from './src/components/Loading';
 
@@ -26,7 +26,7 @@ export default function App() {
       translucent
     />
 
-    { fontsLoaded ? <Home /> : <Loading />}
+    { fontsLoaded ? <Routes /> : <Loading />}
       
     </Brackground>
   );
